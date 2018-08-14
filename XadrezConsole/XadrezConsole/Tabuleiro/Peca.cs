c02900db61efa9a8fa5a1d08469144cfbc1308e9
ceco96.edu@gmail.com <ceco96.edu@gmail.com>
2018-08-14 20:01:31 -0300

    Implementação do tabuleiro.

---
+++ XadrezConsole/XadrezConsole/Tabuleiro/Peca.cs
@@ -0,0 +1,20 @@
+﻿using XadrezConsole.Tabuleiro;
+
+namespace Tabuleiro
+{
+    class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public int QtdMovimentos { get; protected set; }
+        public Tab Tab { get; protected set; }
+
+        public Peca(Posicao posicao, Cor cor, Tab tab)
+        {
+            this.Posicao = posicao;
+            this.Cor = cor;
+            this.Tab = tab;
+            this.QtdMovimentos = 0;
+        }
+    }
+}
---
+++ XadrezConsole/XadrezConsole/Tabuleiro/Tab.cs
@@ -0,0 +1,18 @@
+﻿using Tabuleiro;
+
+namespace XadrezConsole.Tabuleiro
+{
+    class Tab
+    {
+        private int Linhas { get; set; }
+        private int Colunas { get; set; }
+        private Peca[,] Pecas;
+
+        public Tab(int linhas, int colunas)
+        {
+            this.Linhas = linhas;
+            this.Colunas = colunas;
+            this.Pecas = new Peca[linhas, colunas];
+        }
+    }
+}
